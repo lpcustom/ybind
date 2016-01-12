ab9b9c2d3458d264ad70aeb7d8d9d60cc65124c4
Randy Yates <randyy@intersynq.com>
2016-01-12 03:20:07 -0500

    textareas

--- src/ybind.js
+++ src/ybind.js
@@ -4,7 +4,12 @@ var ybind = {
     bindFromReady: false,
     bindToReady: false,
     ready: false,
-
+    changeEvent: function(ele) {
+        var key     = ele.getAttribute('data-bind-to');
+        v = ele.value;
+        ybind.set(key, v);
+        ybind.apply();
+    },
     startBindTo: function() {
         var elements = document.querySelectorAll('[data-bind-to]');
         for(var i = 0; i < elements.length; i++) {
@@ -12,53 +17,32 @@ var ybind = {
             var key     = elements[i].getAttribute('data-bind-to');
             var value   = elements[i].value;
             var obj     = { key : key, value: value, id: id };
-
             ybind.bindObjs.push(obj);
-
             elements[i].setAttribute("data-yid", "yb-" + id);
+            var v = null;
 
+            var ele = elements[i];
             if(window.addEventListener) {
-                elements[i].addEventListener('change', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
-                elements[i].addEventListener('keyup', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
-                elements[i].addEventListener('paste', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
+                elements[i].addEventListener('change', function() { ybind.changeEvent(ele); });
+                elements[i].addEventListener('keyup', function() { ybind.changeEvent(ele); });
+                elements[i].addEventListener('paste', function() { ybind.changeEvent(ele); });
             } else if(window.attachEvent) {
-                elements[i].attachEvent('onchange', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
-                elements[i].attachEvent('onkeyup', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
-                elements[i].attachEvent('onpaste', function() {
-                    ybind.set(key, this.value);
-                    ybind.apply();
-                });
+                elements[i].attachEvent('onchange', function() { ybind.changeEvent(ele); });
+                elements[i].attachEvent('onkeyup', function() { ybind.changeEvent(ele); });
+                elements[i].attachEvent('onpaste', function() { ybind.changeEvent(ele); });
             }
         }
         ybind.bindToReady = true;
     },
-
     startBindFrom: function() {
         var elements = document.querySelectorAll('[data-bind-from]');
         for(var i = 0; i < elements.length; i++) {
             var id = ybind.getNextId();
             ybind.bindObjs.push({ key : elements[i].getAttribute('data-bind-from'), value: elements[i].innerHTML, id: id });
             elements[i].setAttribute("data-yid", "yb-" + id);
-            console.log(elements[i]);
         }
         ybind.bindFromReady = true;
     },
-
     apply: function() {
         for(var i = 0; i < ybind.bindObjs.length; i++) {
             element = document.querySelector('[data-yid="yb-' + ybind.bindObjs[i].id + '"]');
@@ -71,13 +55,11 @@ var ybind = {
             }
         }
     },
-
     getNextId: function() {
         var old = ybind.nextId;
         ybind.nextId++;
         return old;
     },
-
     set: function(key, value) {
         var exists = false;
         for(var i = 0; i < ybind.bindObjs.length; i++) {
@@ -91,7 +73,6 @@ var ybind = {
             ybind.bindObjs.push({key: key, value: value, id: id});
         }
     },
-
     _start: function() {
         "use strict";
         document.addEventListener('DOMContentLoaded', function(event) {
@@ -99,7 +80,6 @@ var ybind = {
             ybind.startBindFrom();
         });
     },
-
     ready:function(callback) {
         var interval = setInterval(function() {
             if(ybind.bindFromReady && ybind.bindToReady) {
